d1fe5df03a47f84e972fca2c14d48ce58c0385de
Andrey Kerchin <153109443+AKercha1@users.noreply.github.com>
2025-08-11 03:18:17 +0500

    Default tags for WebHooks (#113)
    * + SMTP attachments
    
    * Default Tags for WebHooks call
    
    * Review notes
    
    ---------
    
    Co-authored-by: Andrey Kerchin <akerchin@viacode.com>

--- AiCoreApi/Common/RequestAccessor.cs
+++ AiCoreApi/Common/RequestAccessor.cs
@@ -98,10 +98,28 @@ namespace AiCoreApi.Common
         public int? AgentId { get; set; }
         public MessageDialogViewModel? MessageDialog { get; set; }
 
-        public List<int> Tags => (TagsString ?? "")
-            .Split(',')
-            .Select(item => Convert.ToInt32(item))
-            .ToList();
+        private List<int>? _tags;
+        public List<int> Tags
+        {
+            get
+            {
+                if (_tags == null)
+                {
+                    _tags = (TagsString ?? "")
+                        .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                        .Select(item => int.TryParse(item.Trim(), out var value) ? (int?)value : null)
+                        .Where(val => val.HasValue)
+                        .Select(val => val.Value)
+                        .ToList();
+                }
+                return _tags;
+            }
+            set
+            {
+                _tags = value;
+                TagsString = _tags == null ? "" : string.Join(",", _tags);
+            }
+        }
 
         public LoginTypeEnum LoginType => Enum.TryParse<LoginTypeEnum>(LoginTypeString, out var loginType) ? loginType : LoginTypeEnum.Password;
 
