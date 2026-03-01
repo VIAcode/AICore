45d1f47d564017fb9211508d29da82617d752200
Andrey Kerchin <andrey.kerchin@gmail.com>
2026-03-01 12:24:15 +0400

    -

--- AiCoreApi/Program.cs
+++ AiCoreApi/Program.cs
@@ -34,6 +34,7 @@ public class Program
 
 
 
+
                       services.AddTransient<IDbQuery, DbQuery>();
                       services.AddTransient<IDataSourceProvider, DataSourceProvider>();
                       services.AddTransient<Db>();
