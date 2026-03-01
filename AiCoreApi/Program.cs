b9cf16dce55dc4474de3419aed8b32e75e284149
Andrey Kerchin <andrey.kerchin@gmail.com>
2026-03-01 06:31:25 +0400

    -

--- AiCoreApi/Program.cs
+++ AiCoreApi/Program.cs
@@ -30,6 +30,7 @@ public class Program
                       var config = new Config();
                       services.AddSingleton(config);
 
+
                       services.AddTransient<IDbQuery, DbQuery>();
                       services.AddTransient<IDataSourceProvider, DataSourceProvider>();
                       services.AddTransient<Db>();
